b34deaef8b3a43f58fdd5cb9da6cfe71a9c23bbf
ischoolselearning <ischools.elearning@gmail.com>
2026-09-25 12:31:26 -0400

    initial commit

---
+++ client/next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export', // Tells Next.js to generate static files
+
+  // Required: GitHub Pages handles assets via subdirectories (e.g., username.github.io/repo-name)
+  basePath: isProd ? '/abrb' : '',
+  assetPrefix: isProd ? '/abrb/' : '',
+
+  // Required: GitHub Pages doesn't support Next.js default Image Optimization API
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
